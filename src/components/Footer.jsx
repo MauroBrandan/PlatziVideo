b6ac75c705b4637a2b6a9942ffd22958b0c6b4c5
Mauro Brandan <maurobrandan.mb@gmail.com>
2021-07-08 20:05:19 -0300

    Footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import '../assets/styles/components/Footer.scss'
+
+const Footer = () => (
+	<footer className='footer'>
+		<div className='footer__links'>
+			<a href='/'>Terminos de uso</a>
+			<a href='/'>Declaración de privacidad</a>
+			<a href='/'>Centro de ayuda</a>
+		</div>
+	</footer>
+)
+
+export default Footer
--- src/containers/app.jsx
+++ src/containers/app.jsx
@@ -2,15 +2,13 @@ import React from 'react'
 
 import '../assets/styles/app.css'
 
-import Header from '../components/Header'
-import Search from '../components/Search'
-import Carousel from '../components/Carousel'
-import Form from '../components/Form'
+import Footer from '../components/Footer'
+// import Home from './Home'
 
 const app = () => {
 	return (
 		<div>
-			<Form page={'register'} />
+			<Footer />
 		</div>
 	)
 }
