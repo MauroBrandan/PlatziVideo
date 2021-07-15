ab3ceedb01a3f22804c6e613f4589bf0807b325a
Mauro Brandan <maurobrandan.mb@gmail.com>
2021-07-14 22:35:15 -0300

    Componente: Loader

---
+++ src/components/Loader.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+import '../assets/styles/components/Loader.scss'
+
+const Loader = () => {
+	return (
+		<div className='Loader'>
+			<div className='Loader__bars'>
+				<div></div>
+				<div></div>
+				<div></div>
+			</div>
+		</div>
+	)
+}
+
+export default Loader
