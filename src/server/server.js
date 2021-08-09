c469a37f5b728a79f945c8a7baf24211ba0b2f04
Mauro Brandan <maurobrandan.mb@gmail.com>
2021-08-09 13:05:04 -0300

    Definicion de la funcion principal para realizar el renderizado desde el servidor

--- src/server/server.js
+++ src/server/server.js
@@ -1,6 +1,16 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import webpack from 'webpack'
+import React from 'react'
+import { renderToString } from 'react-dom/server'
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
+import { StaticRouter } from 'react-router-dom'
+import { renderRoutes } from 'react-router-config'
+
+import serverRoutes from '../frontend/routes/serverRoutes'
+import reducer from '../frontend/reducers'
+import initialState from '../frontend/initialState'
 
 const app = express()
 
@@ -22,30 +32,45 @@ if (ENV === 'development') {
 	app.use(webpackHotMiddleware(compiler))
 }
 
-app.get('*', (req, res) => {
-	res.send(`
-	<!DOCTYPE html>
-	<html lang="en">
-		<head>
-			<meta charset="UTF-8" />
-			<meta http-equiv="X-UA-Compatible" content="IE=edge" />
-			<meta name="viewport" content="width=device-width, initial-scale=1.0" />
-			<link rel="preconnect" href="https://fonts.googleapis.com" />
-			<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-			<link
-				href="https://fonts.googleapis.com/css2?family=Mulish:wght@300;500;700&display=swap"
-				rel="stylesheet"
-			/>
-			<link rel="stylesheet" href="assets/app.css" type="text/css">
-			<title>Platzi Video</title>
-		</head>
-		<body>
-			<div id="app"></div>
-			<script src="assets/app.js" type="text/javascript"></script>
-		</body>
-	</html>
-	`)
-})
+const renderApp = (req, res) => {
+	const store = createStore(reducer, initialState)
+	const html = renderToString(
+		<Provider store={store}>
+			<StaticRouter location={req.url} context={{}}>
+				{renderRoutes(serverRoutes)}
+			</StaticRouter>
+		</Provider>
+	)
+
+	res.send(setResponse(html))
+}
+
+const setResponse = (html) => {
+	return `
+		<!DOCTYPE html>
+		<html lang="en">
+			<head>
+				<meta charset="UTF-8" />
+				<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+				<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+				<link rel="preconnect" href="https://fonts.googleapis.com" />
+				<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+				<link
+					href="https://fonts.googleapis.com/css2?family=Mulish:wght@300;500;700&display=swap"
+					rel="stylesheet"
+				/>
+				<link rel="stylesheet" href="assets/app.css" type="text/css">
+				<title>Platzi Video</title>
+			</head>
+			<body>
+				<div id="app">${html}</div>
+				<script src="assets/app.js" type="text/javascript"></script>
+			</body>
+		</html>
+		`
+}
+
+app.get('*', renderApp)
 
 app.listen(PORT, (err) => {
 	err ? console.log(err) : console.log(`Server running on http://localhost:${PORT}`)
