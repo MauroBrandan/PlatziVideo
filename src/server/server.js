37a29f46cc49d6b3cdb1609d008e10e701b9ea3a
Mauro Brandan <maurobrandan.mb@gmail.com>
2021-08-09 11:23:06 -0300

    Servir React con Express

--- src/server/server.js
+++ src/server/server.js
@@ -23,7 +23,28 @@ if (ENV === 'development') {
 }
 
 app.get('*', (req, res) => {
-	res.send({ hello: 'Hello World' })
+	res.send(`
+	<!DOCTYPE html>
+	<html lang="en">
+		<head>
+			<meta charset="UTF-8" />
+			<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+			<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+			<link rel="preconnect" href="https://fonts.googleapis.com" />
+			<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+			<link
+				href="https://fonts.googleapis.com/css2?family=Mulish:wght@300;500;700&display=swap"
+				rel="stylesheet"
+			/>
+			<link rel="stylesheet" href="assets/app.css" type="text/css">
+			<title>Platzi Video</title>
+		</head>
+		<body>
+			<div id="app"></div>
+			<script src="assets/app.js" type="text/javascript"></script>
+		</body>
+	</html>
+	`)
 })
 
 app.listen(PORT, (err) => {
