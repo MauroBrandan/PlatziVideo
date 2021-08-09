a4634a1d214292f8cd7789acf25fe197ba255a3a
Mauro Brandan <maurobrandan.mb@gmail.com>
2021-08-09 12:05:35 -0300

    Abstrayendo React Router, creando history y haciendo initialState mas accesible

--- src/frontend/index.js
+++ src/frontend/index.js
@@ -2,18 +2,23 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
 import { createStore, compose } from 'redux'
+import { Router } from 'react-router-dom'
+import { createBrowserHistory } from 'history'
 
 import reducer from './reducers'
 import App from './routes/App'
-import { initialState } from '../../initialState.json'
+import initialState from './initialState'
 
+const history = createBrowserHistory()
 const container = document.getElementById('app')
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
 const store = createStore(reducer, initialState, composeEnhancers())
 
 ReactDOM.render(
 	<Provider store={store}>
-		<App />
+		<Router history={history}>
+			<App />
+		</Router>
 	</Provider>,
 	container
 )
